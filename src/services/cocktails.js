39a22d05dca0726d342ea76b3b6c9343f07f773e
ChrisMak <cmakride@gmail.com>
2022-03-25 10:52:56 -0400

    NavBar Update

--- src/services/cocktails.js
+++ src/services/cocktails.js
@@ -2,7 +2,7 @@ import * as tokenService from './tokenService'
 const BASE_URL = `/api/cocktails`
 
 function create(cocktail){
-    console.log(cocktail)
+    console.log("SERVICES",cocktail)
     return fetch(BASE_URL, {
         method: 'POST',
         headers: {'content-type': 'application/json'},
