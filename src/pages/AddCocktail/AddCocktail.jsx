028a76bdb4328176dbf02f0ba39484c6bd1d68ec
choijyoung <choi.jae.y@gmail.com>
2022-03-25 09:29:42 -0400

    form

--- src/pages/AddCocktail/AddCocktail.jsx
+++ src/pages/AddCocktail/AddCocktail.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 function AddCocktail(props){
     const formElement = useRef()
     const [formData, setFormData] = useState({
-        name: "enter name"
+        name: ""
     })
 
     const handleChange = (evt) => {
@@ -32,7 +32,34 @@ function AddCocktail(props){
             name="name"
             onChange={handleChange}
             value={formData.name}
-            />
+            /> <br /> 
+            <label htmlFor="ingredients-input">Ingredients:</label>
+            <input 
+            type="text"
+            name="ingredients"
+            onChange={handleChange}
+            value={formData.ingredients}
+            /> <br />
+            <label htmlFor="method-input">Method:</label>
+            <select name="method" onChange={handleChange} value={formData.method}>
+                <option>Shake</option>
+                <option>Mixing Glass</option>
+                <option>Build in Glass</option>
+            </select> <br />
+            <label htmlFor="garnish-input">Garnish:</label>
+            <input 
+            type="text"
+            name="garnish"
+            onChange={handleChange}
+            value={formData.garnish}
+            /> <br />
+            <label htmlFor="servedin-input">Served in:</label>
+            <select name="servedIn" onChange={handleChange} value={formData.servedIn}>
+                <option>Martini Glass</option>
+                <option>Rocks Glass</option>
+                <option>Rocks Glass Ice</option>
+                <option>High Ball</option>
+            </select> <br />
             <button type='submit'>Add Cocktail</button>
         </form>
         </>
