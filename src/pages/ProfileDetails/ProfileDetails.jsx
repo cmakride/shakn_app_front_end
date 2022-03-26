f65787b121ceca2ce47aac8ee74aad220e17d5ef
AJKAR <karczewskiamanda@gmail.com>
2022-03-25 21:06:02 -0500

    route profiles to details page

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -13,20 +13,3 @@ const ProfileDetails = (profile) => {
 }
 
 export default ProfileDetails;
-
-
-// const CocktailDetail = (cocktail) => {
-//     const location = useLocation()
-//     const cocktails = location.state.cocktail
-
-
-//     console.log(cocktails)
-//     return ( 
-//         <>
-//         <h1>Details:</h1>
-//         <h2>{cocktails.name}</h2>
-//         </>
-//      );
-// }
- 
-// export default CocktailDetail;
