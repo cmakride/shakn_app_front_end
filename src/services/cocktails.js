23583557fdbc9d28093f4c0c318160e6b9a397bd
ChrisMak <cmakride@gmail.com>
2022-03-27 13:22:51 -0400

    Create Cocktail Update, Link with Profile

--- src/services/cocktails.js
+++ src/services/cocktails.js
@@ -5,7 +5,10 @@ function create(cocktail){
     console.log("SERVICES",cocktail)
     return fetch(BASE_URL, {
         method: 'POST',
-        headers: {'content-type': 'application/json'},
+        headers: { 
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+    },
         body: JSON.stringify(cocktail)
                              
     })
