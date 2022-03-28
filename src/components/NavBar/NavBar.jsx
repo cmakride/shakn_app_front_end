b4454d6a1199f834d709f77947247c8f5a459eaa
choijyoung <choi.jae.y@gmail.com>
2022-03-28 15:11:16 -0400

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,81 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
-      {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-            <li><Link to="/add">Add Cocktail</Link></li>
-            <li><Link to="/cocktails">View Cocktails</Link></li>
+      {user ? (
+        <nav className="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+          <div>
+            <span className="text-2xl cursor-pointer mr-1 pt-2">
+            <ion-icon name="wine-outline"></ion-icon>
+              Shakn
+            </span>
+          </div>
+          <ul className="md:flex md:items-center md:pb-0 pb-12 abosulite md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in">
+            <li className="mx-4 my-6 md:my-0">
+              <NavLink
+                to="/add"
+                className="text-xl hover:text-orange-600 duration-300"
+              >
+                Add Cocktail
+              </NavLink>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <NavLink
+                to="/cocktails"
+                className="text-xl hover:text-orange-600 duration-300"
+              >
+                View Cocktails
+              </NavLink>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <NavLink
+                to="/profiles"
+                className="text-xl hover:text-orange-600 duration-300"
+              >
+                Profiles
+              </NavLink>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <NavLink
+                to="/changePassword"
+                className="text-xl hover:text-orange-600 duration-300"
+              >
+                Change Password
+              </NavLink>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <NavLink
+                to=""
+                onClick={handleLogout}
+                className="text-xl hover:text-orange-600 duration-300"
+              >
+                LOG OUT
+              </NavLink>
+            </li>
           </ul>
         </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+      ) : (
+        <nav className="p-5 bg-white shaodw md:flex md:items-center md:justify-between">
+          <div>
+            <span className="text-2xl cursor-pointer">
+              <ion-icon name="wine-outline"></ion-icon>
+              Shakn
+            </span>
+          </div>
+          <ul className="md:flex md:items-center md:pb-0 pb-12 abosulite md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9">
+            <li className="mx-4 my-6 md:my-0">
+              <Link to="/login">Log In</Link>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <Link to="/signup">Sign Up</Link>
+            </li>
           </ul>
         </nav>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,66 +1,57 @@
-import React, { useState, useRef, useEffect } from "react"
-import { Link, useLocation } from 'react-router-dom'
-
+import React, { useState, useRef, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function EditProfile(props) {
-  const location = useLocation()
-  const [validForm, setValidForm] = useState(true)
-  const formElement = useRef()
-	const [formData, setFormData] = useState(location.state.profile)({
-		name: '',
-	})
+  const location = useLocation();
+  const [validForm, setValidForm] = useState(true);
+  const formElement = useRef();
+  const [formData, setFormData] = useState(location.state.profile)({
+    name: "",
+  });
 
-
-  const handleChange = evt => {
-    setFormData({...formData, [evt.target.name]: evt.target.value })
-  }
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
 
   useEffect(() => {
-		formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
-	}, [formData])
+    formElement.current.checkValidity()
+      ? setValidForm(true)
+      : setValidForm(false);
+  }, [formData]);
 
-	const handleSubmit = evt => {
-		evt.preventDefault()
-		const profileFormData = new FormData()
-		profileFormData.append('name', formData.name)
-		props.handleUpdateProfile(profileFormData)
-	}
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const profileFormData = new FormData();
+    profileFormData.append("name", formData.name);
+    props.handleUpdateProfile(profileFormData);
+  };
 
-	return (
-		<>
-			<h1>Edit Profile</h1>
-			<form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="name-input">
-						Name (required)
-					</label>
-					<input 
-						type="text"
-						id="name-input"
-						name="name"
+  return (
+    <>
+      <h1>Edit Profile</h1>
+      <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name-input">Name (required)</label>
+          <input
+            type="text"
+            id="name-input"
+            name="name"
             value={formData.name}
             onChange={handleChange}
-						required
-					/>
-				</div>
-				<div>
-					<button
-						type="submit"
-            disabled={!validForm}
-					>
-						Save
-					</button>
-				</div>
+            required
+          />
+        </div>
         <div>
-					<Link
-						to="/"
-					>
-						Cancel
-					</Link>
-				</div>
-			</form>
-		</>
-	)
+          <button type="submit" disabled={!validForm}>
+            Save
+          </button>
+        </div>
+        <div>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default EditProfile
+export default EditProfile;
