7e75d051dd51a4d2cbf0cfb14c4dd1493d4952b9
choijyoung <choi.jae.y@gmail.com>
2022-03-28 11:41:09 -0400

    form view

--- src/pages/CocktailDetails/CocktailDetails.jsx
+++ src/pages/CocktailDetails/CocktailDetails.jsx
@@ -10,8 +10,12 @@ const CocktailDetail = (cocktail) => {
     console.log(cocktails)
     return ( 
         <>
-        <h1>Details:</h1>
-        <h2>{cocktails.name}</h2>
+        <img src={cocktails.image} alt="Cocktail" />
+        <h1>{cocktails.name}</h1>
+        <h2>Method: {cocktails.method}</h2>
+        <h2>Garnish: {cocktails.garnish}</h2>
+        <h2>Served in: {cocktails.served_in}</h2>
+        <h2>Ingredients: {cocktails.ingredients}</h2>
         </>
      );
 }
