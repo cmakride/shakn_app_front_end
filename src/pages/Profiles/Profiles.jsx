e50560ea73568fdc6474ac6a19e33a06a6e13ba8
AJKAR <karczewskiamanda@gmail.com>
2022-03-31 15:01:44 -0500

    avatar on profile list

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -19,15 +19,15 @@ const Profiles = () => {
           <>
     
             {profiles.map((profile, idx) => (
+      <div className= "w-40 p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1 md:flex md:flex-wrap">
               <Link
                 key={idx}
                 to='/profile'
                 state={ profile }
               >
-                <div className= "w-40 p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1 md:flex md:flex-wrap">
                 <h4 className="mt-8 text-center text-xl font-bold">{profile.name}</h4><img className="object-scale-down h-26 w-50" src={profile.photo} alt="avatar" />
-                </div>
               </Link>
+                </div>
             ))}
           </>
           :
