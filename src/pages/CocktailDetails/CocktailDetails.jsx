05ac1511a51dc87dd212a93d3ed407eb09687305
ChrisMak <cmakride@gmail.com>
2022-04-21 17:48:00 -0400

    Warnings Handle

--- src/pages/CocktailDetails/CocktailDetails.jsx
+++ src/pages/CocktailDetails/CocktailDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState,useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import Comments from '../../components/Comments/Comments';
 import Reviews from '../../components/Reviews/Reviews';
